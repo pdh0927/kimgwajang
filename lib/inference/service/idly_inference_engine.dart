763f5267f529afc551b37bf169df0d1bccdc4c31
Hyeonsik Song <hssong@beingidly.com>
2023-08-26 08:44:45 +0900

    Add IdlyInferenceEngine & replace to them
    Signed-off-by: Hyeonsik Song <hssong@beingidly.com>

---
+++ lib/inference/service/idly_inference_engine.dart
@@ -0,0 +1,30 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:kimgwajang/inference/service/inference_engine.dart';
+
+class IdlyGptInferenceEngine implements InferenceEngine {
+  final Uri REQUEST_URI = Uri.parse('http://dev.beingidly.com/');
+
+  @override
+  Future<String> inference(String argument) async {
+    var completer = Completer<String>();
+    var response = http.post(
+      REQUEST_URI,
+      body: argument,
+    );
+
+    response.then((response) {
+      final decodeData = utf8.decode(response.bodyBytes);
+      try {
+        String result = decodeData;
+        completer.complete(result);
+      } catch (except) {
+        completer.completeError(decodeData);
+      }
+    });
+
+    return completer.future;
+  }
+}
