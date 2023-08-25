60f7c48677e25970d8f48dfd394505b820f99f51
dh <pdh980927@naver.com>
2023-08-25 14:38:15 +0900

    refactor: change model name

--- lib/complaint/const/data.dart
+++ lib/complaint/const/data.dart
@@ -1,9 +1,9 @@
 import 'package:kimgwajang/complaint/model/complaint_model.dart';
 
 // 임시 민원 데이터
-final List<Complaint> complaints = [
-  Complaint(title: '민원 제목 1', content: '민원 내용 1', reply: '답변 1'),
-  Complaint(title: '민원 제목 2', content: '민원 내용 2', reply: '답변 2'),
-  Complaint(title: '민원 제목 2', content: '민원 내용 2', reply: '답변 2'),
-  Complaint(title: '민원 제목 2', content: '민원 내용 2', reply: '답변 2'),
+final List<ComplaintModel> complaints = [
+  ComplaintModel(title: '민원 제목 1', content: '민원 내용 1', reply: '답변 1'),
+  ComplaintModel(title: '민원 제목 2', content: '민원 내용 2', reply: '답변 2'),
+  ComplaintModel(title: '민원 제목 2', content: '민원 내용 2', reply: '답변 2'),
+  ComplaintModel(title: '민원 제목 2', content: '민원 내용 2', reply: '답변 2'),
 ];
